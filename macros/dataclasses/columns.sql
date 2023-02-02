6b1f3543491b2fadab265054e28dfad1113b8cf8
Teghan Nightengale <tnightengale@gmail.com>
2023-02-02 12:27:49 -0500

    wip: configure columns w. project vars (#2)

--- macros/dataclasses/columns.sql
+++ macros/dataclasses/columns.sql
@@ -4,17 +4,68 @@
 
 {% macro default__columns() %}
 
+{% set activity_schema_required_column_aliases = var(
+    "activity_schema_required_column_aliases",
+    dict(
+        ts = "ts",
+        customer = "customer",
+        activity = "activity",
+        activity_occurrence = "activity_occurrence",
+        activity_repeated_at = "activity_repeated_at"
+    )
+) %}
+
+{% set required_columns = [
+    "ts",
+    "customer",
+    "activity",
+    "activity_occurrence",
+    "activity_repeated_at"
+] %}
+
+{% set required_columns_provided = activity_schema_required_column_aliases.keys() | list %}
+{% if required_columns_provided != required_columns %}
+    {% set message %}
+    "Project variable 'activity_schema_required_column_aliases' must contain the following keys: " {{ required_columns }},
+    "Got: " {{ required_columns_provided }}
+    {% endset %}
+    {{ exceptions.raise_compiler_error(message)}}
+{% endif %}
+
+{% set activity_schema_primary_activity_columns = var(
+    "activity_schema_primary_activity_columns",
+    [
+        "activity_id",
+        "activity",
+        "anonymous_customer_id",
+        "feature_json",
+        "revenue_impact",
+        "link"
+    ]
+) %}
+
+{% set activity_schema_appended_activities_columns = var(
+    "activity_schema_appended_activities_columns",
+    [
+        dict(
+            name = "feature_json",
+            aggregation = "min"
+        ),
+        dict(
+            name = "ts",
+            aggregation = "min"
+        )
+    ]
+) %}
+
 {% do return(namespace(
-    activity_id = var("activity_stream_activity_id_col_name", "activity_id"),
-    ts = var("activity_stream_ts_col_name", "ts"),
-    customer = var("activity_stream_customer_col_name", "customer"),
-    activity = var("activity_stream_activity_col_name", "activity"),
-    anonymous_customer_id = var("activity_stream_anonymous_customer_id_col_name", "anonymous_customer_id"),
-    feature_json = var("activity_stream_feature_json_col_name", "feature_json"),
-    revenue_impact = var("activity_stream_revenue_impact_col_name", "revenue_impact"),
-    link = var("activity_stream_link_col_name", "link"),
-    activity_occurrence = var("activity_stream_activity_occurrence_col_name", "activity_occurrence"),
-    activity_repeated_at = var("activity_stream_activity_repeated_at_col_name", "activity_repeated_at")
+    ts = activity_schema_required_column_aliases["ts"],
+    customer = activity_schema_required_column_aliases["customer"],
+    activity = activity_schema_required_column_aliases["activity"],
+    activity_occurrence = activity_schema_required_column_aliases["activity_occurrence"],
+    activity_repeated_at = activity_schema_required_column_aliases["activity_repeated_at"],
+    primary_activity = activity_schema_primary_activity_columns,
+    appended_activities = activity_schema_appended_activities_columns
 )) %}
 
 {% endmacro %}
