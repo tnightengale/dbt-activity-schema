0b661b0293ead26fd804ad7d84d69ae6590ad695
Teghan Nightengale <tnightengale@gmail.com>
2023-03-13 18:32:09 +0000

    wip: failing agg in before integration test

---
+++ macros/relationships/append_only/aggregate_in_before.sql
@@ -0,0 +1,19 @@
+{% macro aggregate_in_before_join_clause(i) %}
+
+{% set stream = dbt_activity_schema.generate_stream_alias %}
+{% set columns = dbt_activity_schema.columns() %}
+
+(
+    {{ stream(i) }}.{{- columns.ts }} < {{ stream() }}.{{- columns.ts }}
+)
+{% endmacro %}
+
+{% macro aggregate_in_before() %}
+
+{% do return(namespace(
+    name="aggregate_in_before",
+    aggregation_func="count",
+    join_clause=dbt_activity_schema.aggregate_in_before_join_clause
+)) %}
+
+{% endmacro %}
