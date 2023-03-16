53b2c3303f07c8be9a8a7be69b671c2fb9372787
Teghan Nightengale <tnightengale@gmail.com>
2023-03-16 15:32:09 +0000

    feat: aggregate_in_before relationship

--- macros/relationships/append_only/aggregate_in_before.sql
+++ macros/relationships/append_only/aggregate_in_before.sql
@@ -1,6 +1,6 @@
 {% macro aggregate_in_before_join_clause(i) %}
 
-{% set stream = dbt_activity_schema.generate_stream_alias %}
+{% set stream = dbt_activity_schema.alias_stream %}
 {% set columns = dbt_activity_schema.columns() %}
 
 (
