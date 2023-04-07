f4f6c1090e76b755a5c3dc61b4f914ccef5b1612
Will Brown <wibrown@adcadvance.com>
2023-04-07 10:29:27 -0400

    add snowflake min or max

--- macros/utils/aggregations/_min_or_max.sql
+++ macros/utils/aggregations/_min_or_max.sql
@@ -1,20 +1,65 @@
 {% macro _min_or_max(min_or_max, qualified_col) %}
 
+{% if target.type == "snowflake" %}
 {% set aggregation = "min_by" if min_or_max == "min" else "max_by" %}
 {% set qualified_ts_col = "{}.{}".format(
     dbt_activity_schema.appended(), dbt_activity_schema.columns().ts
 ) %}
 
-{# Apply min or max by to the selected column #}
+{# Apply min or max by to the selected column. #}
 {% set aggregated_col %}
-    {{ aggregation }}({{ qualified_col }}, {{ qualified_ts_col }})
+        {{ aggregation }}({{ qualified_col }}, {{ qualified_ts_col }})
 {% endset %}
 
 {# Return output. #}
+{% do return(aggregated_col) %}
+
+{% else %}
+{% set aggregation = "min" if min_or_max == "min" else "max" %}
+{% set column_name = qualified_col.split(".")[-1].strip() %}
+{% set qualified_ts_col = "{}.{}".format(
+    dbt_activity_schema.appended(), dbt_activity_schema.columns().ts
+) %}
+{% set columns = dbt_activity_schema.columns() %}
+
+{# Set type to cast back to after aggregation. #}
+{# TODO: Refactor column abstraction to contain types. #}
+{% if column_name in [columns.ts, columns.activity_repeated_at] %}
+{% set type = dbt.type_timestamp() %}
+{% elif column_name in [columns.activity_occurrence, columns.revenue_impact] %}
+{% set type = dbt.type_numeric() %}
+{% else %} {% set type = dbt.type_string() %}
+{% endif %}
+
+{# Prepend ts column and aggregate. See here for details: https://tinyurl.com/mwfz6xm4 #}
+{% set ts_concatenated_and_aggregated_col %}
+        {{ aggregation }}(
+            {{ dbt.concat([
+                dbt.safe_cast(qualified_ts_col, dbt.type_string()),
+                dbt.safe_cast(qualified_col, dbt.type_string())
+                ]) }}
+            )
+{% endset %}
+
+{# Aggregate ts column before trimming, so it is not required in GROUP BY. #}
+{% set aggregated_ts_col %}
+        {{ aggregation }}( {{ dbt.safe_cast(qualified_ts_col, dbt.type_string()) }} )
+{% endset %}
+
+{# Calculate length of column without prepended & aggregated ts column. #}
+{% set retain_n_rightmost_characters %}
+    {{ dbt.length(ts_concatenated_and_aggregated_col) }} - {{ dbt.length(aggregated_ts_col) }}
+{% endset %}
+
+{# Remove prepended & aggregated ts column. #}
 {% set output %}
-{{ aggregated_col }}
+    {{ dbt.safe_cast(
+        dbt.right(
+            ts_concatenated_and_aggregated_col,
+            retain_n_rightmost_characters
+        ), type) }}
 {% endset %}
 
 {% do return(output) %}
-
+{% endif %}
 {% endmacro %}
