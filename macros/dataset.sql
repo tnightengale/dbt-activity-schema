a1e399d7bd3cc554ae5d026accc95bb93a652939
Bryce Codell <bryce.codell@24analytics.co>
2023-04-08 23:39:54 -0400

    add parse column method for primary and appended columns in dataset

--- macros/dataset.sql
+++ macros/dataset.sql
@@ -42,7 +42,7 @@ with
 filter_activity_stream_using_primary_activity as (
     select
         {% for col in primary_activity.included_columns + primary_activity.required_columns %}
-        {{ primary() }}.{{ col }}{%- if not loop.last -%},{%- endif %}
+        {{ dbt_activity_schema.parse_column(primary(), col) }} as {{ col }}{%- if not loop.last -%},{%- endif %}
         {% endfor %}
 
     from {{ activity_stream }} as {{ primary() }}
@@ -62,11 +62,7 @@ filter_activity_stream_using_primary_activity as (
         {% endfor %}
 
         {% for col in activity.included_columns %}
-            {%- if col not in columns.values() -%}
-                {%- set parsed_col = dbt_activity_schema.json_unpack_key(appended() ~ '.' ~ columns.feature_json, col) -%}
-            {%- else -%}
-                {%- set parsed_col = appended() ~ '.' ~ col -%}
-            {%- endif -%}
+            {%- set parsed_col = dbt_activity_schema.parse_column(appended(), col) -%}
             {% call activity.relationship.aggregation_func() %}
             {{ parsed_col }}
             {% endcall %} as {{ dbt_activity_schema.alias_appended_activity(activity, col) }}
